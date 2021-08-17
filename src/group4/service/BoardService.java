5d1b9b88839b69b84fc7e6810606894e4611e412
shshnn <ssss5489@naver.com>
2021-08-17 17:33:03 +0900

    list-totalcount,getlist service 추가

--- src/group4/service/BoardService.java
+++ src/group4/service/BoardService.java
@@ -2,6 +2,10 @@ package group4.service;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.NamingException;
 
 import group4.comm.DBConnection;
 import group4.dao.BoardDAO;
@@ -14,6 +18,58 @@ public class BoardService {
 		return instance;
 	}
 	private BoardService () {}
+	
+	
+	public int getTotalCount(String search, String searchtxt) {
+		// TODO Auto-generated method stub
+		DBConnection dbconn=DBConnection.getDBConn();
+		Connection conn=null;
+		int totalcount=0;
+		try {
+			conn=dbconn.getConnection();
+			conn.setAutoCommit(false);
+			BoardDAO dao=BoardDAO.getDAO();
+			totalcount=dao.getTotalCount(conn,search,searchtxt);
+			System.out.println("totalcount!!!:"+totalcount);
+			
+			conn.commit();
+		}catch(SQLException|NamingException e)
+		{
+			try {conn.rollback();} catch(SQLException e2) {}
+		}finally {
+			if(conn!=null) try { conn.close();} catch(SQLException e) {}
+		}
+		return totalcount;
+	}
+	
+	
+	public List<BoardDTO> getlist(int startrow,int endrow, String search , String searchtxt) {
+		// TODO Auto-generated method stub
+		
+		DBConnection dbconn=DBConnection.getDBConn();
+		Connection conn=null;
+		List<BoardDTO> list=new ArrayList<BoardDTO>();
+		try {
+				conn=dbconn.getConnection();
+				BoardDAO dao=BoardDAO.getDAO();
+				
+				list=dao.getList(conn,startrow,endrow,search,searchtxt);
+		}catch(SQLException|NamingException e)
+		{
+			System.out.println(e);
+		}finally {
+			if(conn!=null) try { conn.close();} catch(SQLException e) {}
+		}
+		
+		return list;
+	}
+
+	
+	//list
+	
+	
+	
+	
 	
 	public void writeBoard(BoardDTO dto) {
 		DBConnection dbconn=DBConnection.getDBConn();
@@ -35,7 +91,7 @@ public class BoardService {
 		
 	}
 	
-	//파일첨부
+	//
 	public void insertFile(FileDTO dto2) {
 		DBConnection dbconn=DBConnection.getDBConn();
 		Connection conn=null;
@@ -56,7 +112,7 @@ public class BoardService {
 	}
 	
 	
-	   //특정게시글조회
+	   //
 		public BoardDTO detail(int boardno) {
 			DBConnection dbconn = DBConnection.getDBConn();
 			Connection conn = null;
