8ad7632a000bb4185e19c9be966844f49db2a9f3
choisy <beast12511@gmail.com>
2021-08-17 14:24:50 +0900

    login_out_join기능 추가

---
+++ src/group4/controller/LoginAction.java
@@ -0,0 +1,25 @@
+package group4.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import group4.comm.Action;
+import group4.comm.Forward;
+
+public class LoginAction implements Action {
+
+	@Override
+	public Forward execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		Forward forward=new Forward();
+		forward.setForward(true);
+		forward.setPath("login.jsp");
+		return forward;
+	}
+
+}
---
+++ src/group4/service/MemberService.java
@@ -0,0 +1,57 @@
+package group4.service;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.NamingException;
+
+import group4.comm.DBConnection;
+import group4.dao.MemberDAO;
+import group4.dto.MemberDTO;
+
+public class MemberService {
+	private static MemberService memberService=new MemberService();
+	
+	public static MemberService getMemberService() {
+		return memberService;
+		
+	}
+	private MemberService() {}
+	
+	public void JoinMember(MemberDTO dto) {
+		// TODO Auto-generated method stub
+		DBConnection dbconn=DBConnection.getDBConn();
+		Connection conn=null;
+		
+		try {
+			conn=dbconn.getConnection();
+			
+			MemberDAO dao=MemberDAO.getDAO();
+			dao.joinMember(conn,dto);
+		}catch(SQLException |NamingException e) {
+			System.out.println(e);
+		}finally {
+			if(conn!=null) try {conn.close();} catch(SQLException e) {}
+		}
+	}
+	public int login(String id, String pwd) {
+		// TODO Auto-generated method stub
+		DBConnection dbconn=DBConnection.getDBConn();
+		Connection conn=null;
+		int result=0;
+		
+		try {
+			conn=dbconn.getConnection();
+			
+			MemberDAO dao=MemberDAO.getDAO();
+			result=dao.logindata(conn,id,pwd);
+		}catch(SQLException | NamingException e) {
+			System.out.println(e);
+		}finally {
+			if(conn!=null) try {conn.close();} catch(SQLException e) {}
+		}
+		return result;
+	}
+
+	
+}
