c43b501d56cbfb06a54c12a17735b8de16af6d0a
anko <gantri300@gmail.com>
2021-08-19 21:20:14 +0900

    회원만 댓글달수있도록

--- src/group4/controller/ReplyAddAction.java
+++ src/group4/controller/ReplyAddAction.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import group4.comm.Action;
 import group4.comm.Forward;
@@ -17,8 +18,16 @@ public class ReplyAddAction implements Action {
 	public Forward execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
-		request.setCharacterEncoding("utf-8");
+		HttpSession session = request.getSession();
+		String checkid = (String)session.getAttribute("id");
 		
+		Forward f = new Forward();
+		
+		
+		if(checkid!=null) {
+		
+		
+		request.setCharacterEncoding("utf-8");
 		String id = request.getParameter("id");
 		String content = request.getParameter("content");
 		int boardno =Integer.parseInt(request.getParameter("num"));
@@ -31,11 +40,13 @@ public class ReplyAddAction implements Action {
 		BoardService service = BoardService.getService();
 		service.addReply(dto);
 		
-
-		Forward f = new Forward();
 		f.setForward(false);
 		f.setPath("detail.do?boardno="+boardno);
-		
+		}
+		else {
+			f.setForward(true);
+			f.setPath("WEB-INF/board/fail.jsp");
+		}
 		return f;
 		
 	}
